bba22d9c4c358af20c141835bf341def9ee3d35a
Fernando Velarde <f.velarde@hotmail.com>
2019-11-21 18:26:07 -0600

    not sure

--- Menu/Entrees/PterodactylWings.cs
+++ Menu/Entrees/PterodactylWings.cs
@@ -6,5 +6,18 @@ namespace DinoDiner.Menu.Entrees
 {
     class PterodactylWings
     {
+        public double Price { get; set; }
+        public uint Calories { get; set; }
+
+        public List<string> Ingredients
+        {
+            get
+            {
+                List<string> ingredients = new List<string>() { "Bread" };
+                if (peanutButter) ingredients.Add("Peanut Butter");
+                if (jelly) ingredients.Add("Jelly");
+                return ingredients;
+            }
+        }
     }
 }
